678cc5c0e278b5078076b6e68c6eabd31b6c8348
Ed Weld <edweld@gmail.com>
2016-04-13 13:04:00 +0100

    updated routes

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
@@ -8,9 +7,14 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+]).config(function($routeProvider, $locationProvider){
+	$routeProvider.when('/view1',{
+	    controller:'Controller1',
+	    templateUrl:'partials/partial1.html'
+	}).when('/view2',{
+	    controller: 'Controller2',
+	    templateUrl: 'partials/partial2.html'
+	}).otherwise({redirectTo:'/view1'});
+	//$locationProvider.html5Mode(true); //activate HTML5 Mode
+});
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('Controller1', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('Controller2', [function() {
 
   }]);
